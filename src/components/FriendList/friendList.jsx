eb239aa90b9fc474d8c663bcde83f31136dbf356
Niko123454321 <kvbsnsk@gmail.com>
2023-01-18 13:34:14 +0200

    new

--- src/components/FriendList/friendList.jsx
+++ src/components/FriendList/friendList.jsx
@@ -1,12 +1,14 @@
-export const FriendList = ({friends}) => {
+import css from "./friendList.module.css";
+
+export const FriendList = ({ friends }) => {
   return (
-  <ul className="friend-list">
+  <ul className={css.friend_list}>
       {friends.map(friend => {
         return (
-        <li className="item" key={friend.id} >
-            <span className="status" >{friend.isOnline}</span>
-  <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-  <p className="name">{friend.name}</p>
+        <li className={css.item} key={friend.id} >
+            <span className={friend.isOnline ? css.isOnline : css.status} >{friend.isOnline}</span>
+  <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
+  <p className={css.name}>{friend.name}</p>
 </li>
           )
       })}
--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,17 +1,17 @@
-
+import css from "./statistics.module.css";
 
 export const Statistics = ({ title, stats }) => {
   
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
 
-      <ul className="stat-list">
+      <ul className={css.stat_list}>
         
         {stats.map(stat=> {
-          return (<li className="item" key={stat.id}>
-              <span className="label">{stat.label}</span>
-              <span className="percentage">{stat.percentage}%</span>
+          return (<li className={css.item} key={stat.id}>
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}%</span>
             </li>)
           })}
       
@@ -19,3 +19,5 @@ export const Statistics = ({ title, stats }) => {
     </section>
   )
 }
+
+
